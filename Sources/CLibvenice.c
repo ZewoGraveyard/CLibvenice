46d1a51f43ef20071019dabc0edfb21153ee2184
Robert Payne <robertpayne@me.com>
2017-01-04 05:23:04 +1300

    Added co function

--- Sources/CLibvenice.c
+++ Sources/CLibvenice.c
@@ -1,2 +1,20 @@
+#include "dill/cr.h"
+
 void CLibvenice(void) {
+}
+
+int co(void **ptr, size_t len, void *fn, const char *file, int line, void (*routine)(void *)) {
+    sigjmp_buf *ctx;
+    void *stk = (ptr);
+    int h = dill_prologue(&ctx, &stk, len, file, line);
+    
+    if(h >= 0) {
+        if(!dill_setjmp(*ctx)) {
+            DILL_SETSP(stk);
+            routine(fn);
+            dill_epilogue();
+        }
+    }
+    
+    return h;
 }
--- Sources/include/CLibvenice.h
+++ Sources/include/CLibvenice.h
@@ -1,2 +1,4 @@
 #import libdill.h
 #import dsock.h
+
+int co(void **ptr, size_t len, void *fn, const char *file, int line, void (*routine)(void *));
